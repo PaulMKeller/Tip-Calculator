c8a13af3f2b3e35a47d387dcf29c6e130545075d
Paul Keller <Paul@Paul-Kellers-iMac.local>
2012-03-10 19:02:20 +0000

    Initial Commit

---
+++ Tip Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tip Calculator
+//
+//  Created by Paul Keller on 10/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tip Calculator/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Tip Calculator
+//
+//  Created by Paul Keller on 10/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface MainViewController : UIViewController <FlipsideViewControllerDelegate>
+
+@end
